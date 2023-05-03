e8cd70f69e2d0ebb37a2a5955f4b2a556f283658
vishnuprakash <usvishnuprakash>
2023-05-03 15:38:44 +0530

    added live db

--- config/db.go
+++ config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/usvishnuprakash/gin-grom-restful-api/models"
 	"gorm.io/driver/postgres"
@@ -11,14 +12,30 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dns := "user=postgres password=12345678 dbname=gotest port=5432 sslmode=disable"
-	var db *gorm.DB
+
 	var err error
+	host := os.Getenv("DB_HOST")
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	databaseName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	var dns string
+
+	dns = "user=postgres password=12345678 dbname=gotest port=5432 sslmode=disable"
+
+	if os.Getenv("ENV") == "DEV" {
+		dns = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, username, password, databaseName, port)
+	}
+	fmt.Println("env", os.Getenv("ENV"))
+
+	var db *gorm.DB
+
 	db, err = gorm.Open(postgres.Open(dns), &gorm.Config{})
 
 	if err != nil {
 		fmt.Println(err)
-		db, err = gorm.Open(postgres.Open("user=postgres password=12345678 dbname=gotest port=5432 sslmode=disable host=/private/tmp"), &gorm.Config{})
+		db, err = gorm.Open(postgres.Open(dns), &gorm.Config{})
 		db.Exec("CREATE DATABASE gotest")
 
 		fmt.Println(err)
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/usvishnuprakash/gin-grom-restful-api/config"
 	"github.com/usvishnuprakash/gin-grom-restful-api/routes"
 )
 
 func main() {
+
+	err := godotenv.Load(".env.example")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	r := gin.New()
 
@@ -23,4 +30,5 @@ func main() {
 	routes.CampaignRoutes(r)
 	config.Connect()
 	r.Run(":3000")
+
 }
